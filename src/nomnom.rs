c883ee4f9bcfbdc62d5658da5f41a4f071ab688c
Arne <arne@debian-BULLSEYE-live-builder-AMD64>
2022-06-06 17:52:45 +0100

    cleanup

--- src/nomnom.rs
+++ src/nomnom.rs
@@ -2,7 +2,9 @@
 #![no_main]
 
 mod lib;
-use lib::types::*;
+use lib::display::*;
+use lib::framebuffer::*;
+//use lib::types::*;
 
 #[cfg(not(feature = "panic_led"))]
 use panic_halt as _;
@@ -47,55 +49,6 @@ use eg::text::Text;
 
 use tinybmp::Bmp;
 
-fn init_display(
-    display: bsp::pins::Display,
-    clocks: &mut GenericClockController,
-    sercom4: pac::SERCOM4,
-    mclk: &mut pac::MCLK,
-    timer2: pac::TC2,
-    delay: &mut hal::delay::Delay,
-    port: &mut Port,
-) -> Result<Display, ()> {
-    let gclk0 = clocks.gclk0();
-    let tft_spi = bsp::sercom::SPIMaster4::new(
-        &clocks.sercom4_core(&gclk0).ok_or(())?,
-        32.mhz(),
-        spi::Mode {
-            phase: spi::Phase::CaptureOnFirstTransition,
-            polarity: spi::Polarity::IdleLow,
-        },
-        sercom4,
-        mclk,
-        (
-            display.accel_irq.into_pad(port),
-            display.tft_mosi.into_pad(port),
-            display.tft_sck.into_pad(port),
-        ),
-    );
-
-    let mut tft_cs = display.tft_cs.into_push_pull_output(port);
-    tft_cs.set_low()?;
-
-    let tft_dc = display.tft_dc.into_push_pull_output(port);
-    let tft_reset = display.tft_reset.into_push_pull_output(port);
-
-    let tft_backlight = display.tft_backlight.into_function_e(port);
-    let mut pwm2 = pwm::Pwm2::new(
-        &clocks.tc2_tc3(&gclk0).ok_or(())?,
-        1.khz(),
-        timer2,
-        hal::pwm::TC2Pinout::Pa1(tft_backlight),
-        mclk,
-    );
-    pwm2.set_duty(pwm2.get_max_duty());
-
-    let mut display = st7735::ST7735::new(tft_spi, tft_dc, tft_reset, true, false, 160, 128);
-    display.init(delay)?;
-    display.set_orientation(&st7735::Orientation::LandscapeSwapped)?;
-
-    Ok(display)
-}
-
 #[entry]
 fn main() -> ! {
     let mut peripherals = Peripherals::take().unwrap();
@@ -112,16 +65,28 @@ fn main() -> ! {
 
     let mut delay = hal::delay::Delay::new(core.SYST, &mut clocks);
 
-    let mut display = init_display(
-        pins.display,
-        &mut clocks,
-        peripherals.SERCOM4,
-        &mut peripherals.MCLK,
-        peripherals.TC2,
-        &mut delay,
-        &mut pins.port,
-    )
-    .unwrap();
+    let (mut display, _backlight) = pins
+        .display
+        .init(
+            &mut clocks,
+            peripherals.SERCOM4,
+            &mut peripherals.MCLK,
+            peripherals.TC2,
+            &mut delay,
+            &mut pins.port,
+        )
+        .unwrap();
+
+    //let mut display = init_display(
+    //    pins.display,
+    //    &mut clocks,
+    //    peripherals.SERCOM4,
+    //    &mut peripherals.MCLK,
+    //    peripherals.TC2,
+    //    &mut delay,
+    //    &mut pins.port,
+    //)
+    //.unwrap();
 
     //let mut pins = Pins::new(peripherals.PORT).split();
     //let mut adc1 = hal::adc::Adc::adc1(peripherals.ADC1, &mut peripherals.MCLK, &mut clocks, GCLK11);
@@ -174,48 +139,57 @@ fn main() -> ! {
         fb.clear(Rgb565::BLACK).unwrap();
         text.draw(&mut fb).unwrap();
 
-        upload(&fb, &mut display).unwrap();
+        //upload(&fb, &mut display).unwrap();
+
+        display
+            .set_address_window(0, 0, SCREEN_W as u16, SCREEN_H as u16)
+            .map_err(my_err)
+            .unwrap();
+        display
+            .write_pixels(fb.inner.iter().map(|c| c.into_storage()))
+            .map_err(my_err)
+            .unwrap();
     }
 }
 
 #[derive(Debug)]
 struct MyErr {}
 
-fn main_loop(display: &mut Display) -> Result<(), MyErr> {
-    let mut fb = FrameBuffer::new();
+// fn main_loop(display: &mut Display) -> Result<(), MyErr> {
+//     let mut fb = FrameBuffer::new();
+//
+//     let mut console = heapless::String::<32>::new();
+//
+//     let mut frame = 0;
+//     loop {
+//         console.clear();
+//
+//         writeln!(&mut console, "{frame}").unwrap();
+//         let text = Text::new(&console, Point::new(0, 10), text_style());
+//
+//         fb.clear(Rgb565::BLACK).unwrap();
+//         text.draw(&mut fb).unwrap();
+//
+//         upload(&fb, display)?;
+//     }
+//
+//     //let raw_image: Bmp<Rgb565> = Bmp::from_slice(include_bytes!("../ferris.bmp")).unwrap();
+//     //let ferris = Image::new(&raw_image, Point::new(32, 32));
+//     //ferris.draw(&mut display).unwrap();
+//
+//     Ok(())
+// }
 
-    let mut console = heapless::String::<32>::new();
-
-    let mut frame = 0;
-    loop {
-        console.clear();
-
-        writeln!(&mut console, "{frame}").unwrap();
-        let text = Text::new(&console, Point::new(0, 10), text_style());
-
-        fb.clear(Rgb565::BLACK).unwrap();
-        text.draw(&mut fb).unwrap();
-
-        upload(&fb, display)?;
-    }
-
-    //let raw_image: Bmp<Rgb565> = Bmp::from_slice(include_bytes!("../ferris.bmp")).unwrap();
-    //let ferris = Image::new(&raw_image, Point::new(32, 32));
-    //ferris.draw(&mut display).unwrap();
-
-    Ok(())
-}
-
-fn upload(src: &FrameBuffer, dst: &mut Display) -> Result<(), MyErr> {
-    //dst.draw_iter(src.iter_pixels()).unwrap();
-    //let N = SCREEN_H * SCREEN_W / 2;
-
-    dst.set_address_window(0, 0, SCREEN_W as u16, SCREEN_H as u16)
-        .map_err(my_err)?;
-    dst.write_pixels(src.inner.iter().map(|c| c.into_storage()))
-        .map_err(my_err)?;
-    Ok(())
-}
+//  fn upload(src: &FrameBuffer, dst: &mut Display) -> Result<(), MyErr> {
+//      //dst.draw_iter(src.iter_pixels()).unwrap();
+//      //let N = SCREEN_H * SCREEN_W / 2;
+//
+//      dst.set_address_window(0, 0, SCREEN_W as u16, SCREEN_H as u16)
+//          .map_err(my_err)?;
+//      dst.write_pixels(src.inner.iter().map(|c| c.into_storage()))
+//          .map_err(my_err)?;
+//      Ok(())
+//  }
 
 fn my_err<E>(_e: E) -> MyErr {
     MyErr {}
@@ -225,9 +199,9 @@ fn text_style() -> MonoTextStyle<'static, Rgb565> {
     MonoTextStyle::new(&mono_font::ascii::FONT_7X13_BOLD, Rgb565::WHITE)
 }
 
-fn clear(display: &mut Display) {
-    unwrap(display.clear(Rgb565::BLACK))
-}
+//fn clear(display: &mut Display) {
+//    unwrap(display.clear(Rgb565::BLACK))
+//}
 
 fn unwrap<E>(r: Result<(), E>) {
     r.map_err(|_| MyErr {}).unwrap()
